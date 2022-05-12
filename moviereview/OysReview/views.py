4cb77e2f7e3ae16fc66d1cc84181eb6bdd0eaed5
HAO TRAN <quanghao81111@gmail.com>
2022-05-11 17:01:49 -0700

    Movie View Commit

--- moviereview/OysReview/views.py
+++ moviereview/OysReview/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Movie, OysReviewType, Review
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 def index (request):
     return render(request, 'OysReview/index.html')
+
+def movies(request):
+    movie_list = Movie.objects.all()
+    return render (request, 'OysReview/movies.html', 
+    {'movie_list': movie_list})
 
