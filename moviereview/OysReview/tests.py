63c80b018735bd63121110072727894cd300bdf8
HAO TRAN <quanghao81111@gmail.com>
2022-05-11 22:40:37 -0700

    Models Test Commit

--- moviereview/OysReview/tests.py
+++ moviereview/OysReview/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import OysReviewType, Movie, Review
+import datetime
 
 # Create your tests here.
+
+class OysReviewTest(TestCase):
+    def setUp(self):
+        self.type=OysReviewType(typename='Documentary')
+        
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'Documentary')
+
+    def test_tablename(self):
+        self.assertEqual(str(OysReviewType._meta.db_table), 
+        'OysReviewType')
+    
+class MovieTest(TestCase):
+    def setUp(self):
+        self.type = OysReviewType(typename='Documentary')
+        self.User = User(username = 'user1')
+        self.movie = Movie(moviename = 'How To Become a Tyrant', 
+        movietype = self.type, user = self.User, 
+        datepremiered =datetime.date(2012,5,4), 
+        movieurl = 'https://www.netflix.com',
+        description = 'A wonderful documentary movie')
+
+    def test_string(self):
+        self.assertEqual(str(self.movie),'How To Become a Tyrant')
